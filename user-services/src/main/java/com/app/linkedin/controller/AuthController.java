d8de06b4c397ae4290d7b87219c18b6ef3af0efe
Gaurab Sah <gaurab.sah.777@gmail.com>
2024-12-03 14:55:15 +0545

    user-service

---
+++ user-services/src/main/java/com/app/linkedin/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.app.linkedin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.linkedin.dto.LoginRequestDto;
+import com.app.linkedin.dto.SignupRequestDto;
+import com.app.linkedin.dto.UserDto;
+import com.app.linkedin.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signUp(@RequestBody SignupRequestDto signupRequestDto) {
+        UserDto userDto = authService.signUp(signupRequestDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
+        String token = authService.login(loginRequestDto);
+        return ResponseEntity.ok(token);
+    }
+}
---
+++ user-services/src/main/java/com/app/linkedin/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.app.linkedin.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
